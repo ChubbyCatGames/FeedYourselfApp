36b85f9eb26da54ec3e0cff99ebd71f81c9cc150
elibo09 <e.barba.2019@alumnos.urjc.es>
2022-12-10 13:13:45 +0100

    transiciones paginas

---
+++ lib/providers/theme.dart
@@ -0,0 +1,25 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:material_color_utilities/material_color_utilities.dart';
+
+class NoAnimationPageTransitionsBuilder extends PageTransitionsBuilder {
+  const NoAnimationPageTransitionsBuilder();
+
+  @override
+  Widget buildTransitions<T>(
+    PageRoute<T> route,
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return child;
+  }
+}
+
+final pageTransitionsTheme = const PageTransitionsTheme(
+  builders: <TargetPlatform, PageTransitionsBuilder>{
+    TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+    TargetPlatform.windows: NoAnimationPageTransitionsBuilder(),
+  },
+);
